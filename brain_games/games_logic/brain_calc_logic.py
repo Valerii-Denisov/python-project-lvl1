139e6f64cea0808c4f57b2f9e83e600a9f1dd64f
Valerii-Denisov <klerk919191@gmail.com>
2022-01-07 11:19:40 +0300

    edit brain_calc_logic.py

--- brain_games/games_logic/brain_calc_logic.py
+++ brain_games/games_logic/brain_calc_logic.py
@@ -18,7 +18,7 @@ def correct_answer():
     num1 = randint(bound[0], bound[1])
     num2 = randint(bound[0], bound[1])
     math_operator = choice(['+', '-', '*'])
-    print('Question: {0}{2}{1}'.format(num1, num2, math_operator))
+    print('Question: {0} {2} {1}'.format(num1, num2, math_operator))
     if math_operator == '+':
         cor_answer = num1 + num2
     elif math_operator == '-':
