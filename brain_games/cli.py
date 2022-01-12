69962f20e87c14c49febfeb862a876da054f03ee
Valerii-Denisov <klerk919191@gmail.com>
2022-01-12 21:35:09 +0300

    try to fix dist

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,6 +4,6 @@ import prompt
 
 def welcome_user():
     """Asking name of user.Return greeting of user."""
-    name = prompt.string('May I have your name?  ')
+    name = prompt.string('May I have your name? ')
     if name:
         print('Hello, {0}!'.format(name))
